480a8ed02497b48950433a861932714217d8b906
Elton Santos <eltonaxl@hotmail.com>
2023-09-18 04:24:35 -0300

    Create users page

---
+++ src/pages/users.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Users() {
+  return (
+    <div>
+      <h1 className="text-violet-500">Testando</h1>
+      
+      <Button>Click</Button>
+    </div>
+  )
+}
